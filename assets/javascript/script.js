21febdbbc5cdf0830bb1a47d85a6d0ef930d6981
Alex Harris <akharri8@ncsu.edu>
2017-12-19 22:47:45 -0500

    Have conditionals to check if user input is within the answer

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -15,7 +15,8 @@
 // Declare Vars
 var userInput;
 var userInputArray = [];
-var guesses;
+var guessesString;
+var guessesArray = [];
 var guessesLeft = 6;
 var questionsAndAnswers = [
     ["Year Lebron James was drafted?", "2003"],
@@ -24,33 +25,22 @@ var questionsAndAnswers = [
     ["What number was Michael Jordan Drafted?", "two"],
     ["Who won the NBA Championship in 2008", "celtics"]
 ]
-var answerArray = [];
 var randomArrayIndex = questionsAndAnswers[Math.floor(Math.random() * 5)];
-var answerLength = randomArrayIndex[1].length;
 var answer = randomArrayIndex[1].toLowerCase();
+var answerString;
+var answerArray = [];
 var questionText = document.getElementById("question").innerHTML = randomArrayIndex[0] + "\n Number of Inputs: " + answer.length;
-var userInputString = userInputArray.toString().replace(/,/g, "");
+// var userInputString = userInputArray.toString().replace(/,/g, "");
 
 
 
-
-
-// var question1 = questionsAndAnswers[0];
-// var firstProperty = Object.keys(question1)[0];
 
 questionText;
 underscores();
 document.addEventListener("keypress", function() {
     userInput = event.key;
     getRandomQuestion();
-    underscores();
-    for(var i = 0; i < answer.length; i++) {
-        if(answer[i] == userInput) {
-            answerArray[i] = userInput;
-        }
-    }
-    document.getElementById("answer").innerHTML = answerArray.join(" ");
-
+    inputConditions();
 
 
 
@@ -73,12 +63,29 @@ function getRandomQuestion() {
     }
 }
 
-
+// Creates underscores based on the answer length
 function underscores(){
     for(var i = 0; i < answer.length; i++) {
         answerArray[i] = "_";
     }
-    answerArray.join(" ");
-    document.getElementById("answer").innerHTML = answerArray;
+    answerString = answerArray.join(" ");
+    document.getElementById("answer").innerHTML = answerString;
 }
 
+// Creates logic on if the user input is within the answer or not
+function inputConditions() {
+    if(answer.includes(userInput)){
+        for(var i = 0; i < answer.length; i++) {
+            if(answer[i] == userInput) {
+                answerArray[i] = userInput;
+            }
+        }
+        document.getElementById("answer").innerHTML = answerArray.join(" ");
+    } else {
+        guessesLeft--;
+        document.getElementById("guesses-left").innerHTML = guessesLeft;
+        guessesArray.push(userInput);
+        guessesString = guessesArray.join(" ");
+        document.getElementById("guesses").innerHTML = guessesString;
+    }
+}
