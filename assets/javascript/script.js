c94d2328e3dc53a29e0299fde7f7cac28a51e3c0
Alex Harris <akharri8@ncsu.edu>
2017-12-19 21:25:33 -0500

    update

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -15,47 +15,61 @@
 // Declare Vars
 var userInput;
 var userInputArray = [];
+var guesses;
 var guessesLeft = 6;
-var dashes;
 var questionsAndAnswers = [
     ["Year Lebron James was drafted?", "2003"],
     ["First player to average a triple double?", "OscarRobertson"],
-    ["Mascot name of the Charlotte Hornets?", "Hugo"],
-    ["What number was Michael Jordan Drafted?", "Two"],
-    ["Who won the NBA Championship in 2008", "Celtics"]
+    ["Mascot name of the Charlotte Hornets?", "hugo"],
+    ["What number was Michael Jordan Drafted?", "two"],
+    ["Who won the NBA Championship in 2008", "celtics"]
 ]
 var randomArrayIndex = questionsAndAnswers[Math.floor(Math.random() * 5)];
-var answer; randomArrayIndex[1];
 var answerLength = randomArrayIndex[1].length;
-var dashes = "- ".repeat(answerLength);
-var answer; randomArrayIndex[1];
+var answer = randomArrayIndex[1].toLowerCase();
+var questionText = document.getElementById("question").innerHTML = randomArrayIndex[0] + "\n Number of Inputs: " + answer.length;
+var userInputString = userInputArray.toString().replace(/,/g, "");
 
 
-// indexOf(the correct letter) + 1
 
 
 
 // var question1 = questionsAndAnswers[0];
 // var firstProperty = Object.keys(question1)[0];
 
-document.getElementById("question").innerHTML = randomArrayIndex[0];
-document.getElementById("answer").innerHTML = dashes;
+questionText;
 document.addEventListener("keypress", function() {
-    storeUserInput();
-    randomQuestion();
-    answerDashes();
-    for(var i = 0; i < answer.length; i++) {
-        if(userInput === answer.indexOf(i)) {
-            userInput.replace()
+    userInput = event.key;
+    getRandomQuestion();
+    document.getElementById("answer").innerHTML = answer;
+    if(answer.includes(userInput)) {
+        storeUserInput();
+        for(var i = 0; i < answer.length; i++){
+            if(userInput == answer[i]){
+                var letter = document.createElement("span");
+                var textNode = document.createTextNode(userInput);
+                letter.appendChild(textNode);
+                document.getElementById("answer").appendChild(span);
+            }
         }
+    } else {
+        guessesLeft--;
+        document.getElementById("answer").innerHTML += userInputArray;
+        document.getElementById("guesses-left").innerHTML = guessesLeft;
+    }
+    if(userInputString == answer) {
+        guessesLeft = 6;
+        userInputArray = [];
+        getRandomQuestion();
+        questionText;
     }
 
 
 
 
-
-
 })
+
+
 
 // Record user input and store it to an array
 function storeUserInput() {
@@ -65,17 +79,22 @@ function storeUserInput() {
 }
 
 // create a random number gen to get a random nested array
-function randomQuestion() {
+function getRandomQuestion() {
     randomArrayIndex = questionsAndAnswers[Math.floor(Math.random() * 5)];
     if(guessesLeft === 0) {
         document.getElementById("question").innerHTML = randomArrayIndex[0];     
     }
 }
 
-// create a function to output the correct number of string spaces as dashes to match the 
-//      the number of letters in the answer.
-function answerDashes() {
-    document.getElementById("answer").innerHTML = dashes;
-}
 
+// Maybe need to append correct userinputs
+for(var i = 0; i < answer.length; i++){
+    if(userInput == answer[i]){
+        var newP = document.createElement("p");
+        var textNode = document.createTextNode(userInput);
+        newP.appendChild(textNode);
+        document.getElementById("answer").appendChild(newP);
+
+    }
+}
 
